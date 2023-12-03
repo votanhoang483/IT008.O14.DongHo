e5f33e851f0ec1af908d8b92c31a48e1f59dfb10
PhamKhaiHung <22520522@gm.uit.edu.vn>
2023-12-03 23:11:45 +0700

    pomodoro

--- ChildViews/PomodoroClock.xaml.cs
+++ ChildViews/PomodoroClock.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
+using System.Reflection;
 
 namespace DoAn_LT.ChildViews
 {
@@ -20,9 +22,98 @@ namespace DoAn_LT.ChildViews
     /// </summary>
     public partial class PomodoroClock : UserControl
     {
+        int minute_pomodoro = 25;
+        int second_pomodoro = 0;
         public PomodoroClock()
         {
             InitializeComponent();
+            aTimer = new System.Windows.Forms.Timer();
+            aTimer.Tick += new EventHandler(aTimer_Tick);
+            aTimer.Interval = 1000;
+            label1.Content  = output(second_pomodoro);
+            label2.Content = output(minute_pomodoro);
+
+
+        }
+
+        private System.Windows.Forms.Timer aTimer;
+        
+        private void Start_Click(object sender, EventArgs e)
+        {
+           
+            aTimer.Start();
+            //label1.Text  = output(second_pomodoro);
+            //label2.Text = output(minute_pomodoro);
+        }
+        private void Stop_Click(object sender,EventArgs e)
+        {
+            aTimer.Stop();
+        }
+
+        private void aTimer_Tick(object sender, EventArgs e)
+        {
+            if ((second_pomodoro == 0) && (minute_pomodoro > 0))
+            {
+                minute_pomodoro--;
+                label2.Content= output(minute_pomodoro);
+                second_pomodoro = 60;
+            }
+            second_pomodoro--;
+            
+            if ((minute_pomodoro == 0) && (second_pomodoro == 0))
+            {
+                aTimer.Stop();
+            }
+            label1.Content = output(second_pomodoro);
+        }
+        public string output(int x)
+        {
+            if (x >= 10)
+                return x.ToString();
+            else
+                return "0" + x.ToString();
+        }
+        private void Pomodoro_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+             second_pomodoro = 0;
+            minute_pomodoro = 25;
+           
+            label1.Content = output(second_pomodoro);
+            label2.Content = output(minute_pomodoro);
+        }
+        private void Short_Click(object sender, EventArgs e) 
+        {
+            aTimer.Stop();
+            second_pomodoro =0;
+            minute_pomodoro = 5;
+           
+            label1.Content = output(second_pomodoro);
+            label2.Content       = output(minute_pomodoro);
+        }
+        private void Long_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+            second_pomodoro = 0;
+            minute_pomodoro = 10;
+            label1.Content = output(second_pomodoro);
+            label2.Content = output(minute_pomodoro);
+        }
+        private void Up_Click(object sender, EventArgs e)
+        {
+            
+                minute_pomodoro += 1;
+                label2.Content = output(minute_pomodoro);
+           
+        }
+        private void Down_Click(object sender, EventArgs e)
+        {
+            if (minute_pomodoro > 0)
+            {
+                minute_pomodoro -= 1;
+                label2.Content = output(minute_pomodoro);
+            }
+
         }
     }
 }
