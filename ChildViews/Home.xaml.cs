a382905c69ada58cb6ca44919b7734ac07927d30
PhanTranTienHung22520523 <22520523@gm.uit.edu.vn>
2023-12-23 03:01:24 +0700

    ...

--- ChildViews/Home.xaml.cs
+++ ChildViews/Home.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,19 @@ namespace DoAn_LT.ChildViews
         public Home()
         {
             InitializeComponent();
+            MessageBox.Show("Alo");
+            //Storyboard seconds = (Storyboard)giay.FindResource("sbseconds");
+            //seconds.Begin();
+            //seconds.Seek(new TimeSpan(0, 0, 0, DateTime.Now.Second, 0));
+
+            //Storyboard minutes = (Storyboard)phut.FindResource("sbminutes");
+            //minutes.Begin();
+            //minutes.Seek(new TimeSpan(0, 0, DateTime.Now.Minute, DateTime.Now.Second, 0));
+
+            //Storyboard hours = (Storyboard)gio.FindResource("sbhours");
+            //hours.Begin();
+            //hours.Seek(new TimeSpan(0, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, 0));
         }
+       
     }
 }
